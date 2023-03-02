15c8336f610b1c42ac12312f73c5abd87577ddb5
Shrey <shreyv15@gmail.com>
2023-03-02 22:26:48 +0530

    login/logout done

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(
-    cors({
-        origin: true,
-        credentials: true,
-    })
-);
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+const loginRoute = require("./routes/auth");
+const changepassword = require("./routes/changepassword");
 
 
-const loginRoute = require("./routes/login");
 app.use('/users', loginRoute);
+app.use('/users', changepassword);
 
 app.listen(3000, function () {
     console.log("port is active now!!");
